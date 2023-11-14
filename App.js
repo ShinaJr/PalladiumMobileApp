4100c52e636257e1a4592bb99dab300f6bfed15a
Moses Oluwashina  Lasisi <lasisioluwashina@gmail.com>
2023-11-14 17:06:12 +0100

    Palladium app

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import UserScreen from './Screens/UserScreen';
+import UserScreen1 from './Screens/UserScreen1';
 
 export default function App() {
   return (
     <View>
-      <UserScreen/>
+      {/* <UserScreen/> */}
+      <UserScreen1/>
     </View>
   );
 }
